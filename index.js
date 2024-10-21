6bd3f77bfd237f1d196a40fcdfd32f959c4cb578
Georg <georg@example.com>
2024-10-21 11:11:04 +0000

    Изменено приветствие в index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/hello', (req, res) => {
-  const name = req.query.name || 'World';
-  res.send(`Hello, ${name}!`);
-});
-
-app.listen(port, () => {
-  console.log(`Серв запущен на http://localhost:${port}`);
-});
-
+console.log('Привет, мир');
